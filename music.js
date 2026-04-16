712fbf2778773b879aa82143b70a6bbec4672544
0dsec <0dze@pm.me>
2026-04-16 13:54:20 -0400

    update

--- music.js
+++ music.js
@@ -1,10 +1,6 @@
 /* playlist */
 const playlist = [
-  { title: 'someshit', artist: '0daze', file: 'music/level5.wav' },
-   { title: 'IDK', artist: '0daze', file: 'music/IDK.wav' },
-   { title: 'level one', artist: '0daze', file: 'music/level1.wav' },
-   { title: 'level two', artist: '0daze', file: 'music/level2.wav' },
-   { title: 'Boss 1', artist: '0daze', file: 'music/Boss1.wav' },
+  { title: 'loop', artist: '0daze', file: 'music/loop.wav' },
 ];
 
 let currentTrack = 0;
